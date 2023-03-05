9795b554e277dc39e48500cb088b9b2186648b00
Anton Glazyrin <a.glazyrin@spbu.ru>
2023-03-06 01:13:31 +0300

    [update] add hashing

--- TextDuplicateSearch/DuplicateSearch/StrictSearch/StrictDuplicates.py
+++ TextDuplicateSearch/DuplicateSearch/StrictSearch/StrictDuplicates.py
@@ -4,11 +4,11 @@ from TextDuplicateSearch.DataModels.Configs.SearchConfig import SearchConfig
 from TextDuplicateSearch.DataModels.DuplicateCollection import DuplicateCollection
 from TextDuplicateSearch.DataModels.DuplicateCase import DuplicateCase
 from TextDuplicateSearch.DataModels.TextFragment import TextFragment
-from TextDuplicateSearch.StrictSearch import SuffixArray
+from TextDuplicateSearch.DuplicateSearch.StrictSearch import SuffixArray
 from TextDuplicateSearch.TextProcessing.Token import Token
 
 
-class StrictSearch:
+class StrictDuplicates:
     suffix_array: List[int] = []
     lcp_array: List[int] = []
 
@@ -32,17 +32,17 @@ class StrictSearch:
 
     @staticmethod
     def get_duplicate_data(tokens: List[Token], search_config: SearchConfig) -> DuplicateCollection:
-        StrictSearch.suffix_array, StrictSearch.lcp_array = SuffixArray.build_from_tokens(tokens)
+        StrictDuplicates.suffix_array, StrictDuplicates.lcp_array = SuffixArray.build_from_tokens(tokens)
 
         result: DuplicateCollection = DuplicateCollection(tokens)
         marked: List[bool] = [False] * len(tokens)
         cur_idx: int = 1
-        group_interval: StrictSearch.Interval = StrictSearch.Interval()
+        group_interval: StrictDuplicates.Interval = StrictDuplicates.Interval()
 
         # going through suffix array
         while cur_idx < len(marked):
-            if StrictSearch.lcp_array[cur_idx] >= search_config.min_dup_length:
-                if not marked[StrictSearch.suffix_array[cur_idx]]:
+            if StrictDuplicates.lcp_array[cur_idx] >= search_config.min_dup_length:
+                if not marked[StrictDuplicates.suffix_array[cur_idx]]:
                     group_interval.is_nested = False
 
                 if not group_interval.is_active:
@@ -59,29 +59,29 @@ class StrictSearch:
                 dup_case: DuplicateCase = DuplicateCase()
 
                 # forward from starting tokens
-                length: int = min(StrictSearch.lcp_array[group_interval.begin + 1: group_interval.end + 1])
+                length: int = min(StrictDuplicates.lcp_array[group_interval.begin + 1: group_interval.end + 1])
 
                 # trying to go backwards from starting tokens
                 expand: bool = True
                 shift: int = 1
                 while expand:
-                    cur_token: Token = tokens[StrictSearch.suffix_array[group_interval.begin] - shift]
+                    cur_token: Token = tokens[StrictDuplicates.suffix_array[group_interval.begin] - shift]
                     for idx in range(group_interval.begin + 1, group_interval.end + 1):
-                        if tokens[StrictSearch.suffix_array[idx] - shift] != cur_token:
+                        if tokens[StrictDuplicates.suffix_array[idx] - shift] != cur_token:
                             expand = False
 
                     if expand:
                         shift += 1
 
-                    if StrictSearch.suffix_array[group_interval.begin] - shift < 0:
+                    if StrictDuplicates.suffix_array[group_interval.begin] - shift < 0:
                         break
 
                 shift -= 1
 
                 for idx in range(group_interval.begin, group_interval.end + 1):
-                    fragment: TextFragment = TextFragment(tokens[StrictSearch.suffix_array[idx] - shift], tokens[StrictSearch.suffix_array[idx] + length - 1])
+                    fragment: TextFragment = TextFragment(tokens[StrictDuplicates.suffix_array[idx] - shift: StrictDuplicates.suffix_array[idx] + length])
                     dup_case.add_text_fragment(fragment)
-                    marked[StrictSearch.suffix_array[idx] - shift : StrictSearch.suffix_array[idx] + length] = [True] * (shift + length)
+                    marked[StrictDuplicates.suffix_array[idx] - shift: StrictDuplicates.suffix_array[idx] + length] = [True] * (shift + length)
 
                 result.add_case(dup_case)
 
