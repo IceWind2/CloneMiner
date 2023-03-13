212ee8b122ca576fe6fc590eecfe9f5bfe2ffa90
Anton Glazyrin <a.glazyrin@spbu.ru>
2023-03-13 03:34:02 +0300

    [Feature] Add Ukkonens algorithm for edit distance

--- TextDuplicateSearch/DuplicateSearch/FuzzySearch/FragmentSearch.py
+++ TextDuplicateSearch/DuplicateSearch/FuzzySearch/FragmentSearch.py
@@ -7,7 +7,7 @@ from TextDuplicateSearch.TextProcessing.Token import Token
 class FragmentSearch:
     def __init__(self,
                  hashin_func: Callable[[List[Token]], int],
-                 editdistance_func: Callable[[TextFragment, TextFragment], float]) -> None:
+                 editdistance_func: Callable[[TextFragment, TextFragment, float], float]) -> None:
 
         self.hashin_func: Callable[[List[Token]], int] = hashin_func
-        self.editdistance_func: Callable[[TextFragment, TextFragment], float] = editdistance_func
+        self.editdistance_func: Callable[[TextFragment, TextFragment, float], float] = editdistance_func
